cc94a46419a5c917c42e23a72196e6ce030359c4
joelmunu <joel.munu@gmail.com>
2024-02-24 13:58:56 +0000

    Feature: Se ha añadido la función de generar tuberias aleatoriamente en la escena principal, y se ha añadido el suelo del mapa

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,24 +1,30 @@
 extends Node
 
+@export var pipe_scene : PackedScene
+
 var game_running : bool
 var game_over : bool
 var scroll
 var score
 const SCROLL_SPEED : int = 4
 var screen_size : Vector2i
+var ground_height : int
 var pipes : Array
 const PIPE_DELAY : int = 100
 const PIPE_RANGE : int = 200
 
 func _ready():
+	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 	
 func new_game():
-	#reset variables
 	game_running = false
 	game_over = false
 	score = 0
 	scroll = 0
+	pipes.clear()
+	generate_pipes()
 	$Player.reset()
 
 func _input(event):
@@ -35,3 +41,28 @@ func start_game():
 	game_running = true
 	$Player.flying = true
 	$Player.flap()
+	$PipeTimer.start()	
+
+func _process(delta):
+	if game_running:
+		scroll += SCROLL_SPEED
+		if scroll >= screen_size.x:
+			scroll = 0
+		$Ground.position.x = -scroll
+		for pipe in pipes:
+			pipe.position.x -= SCROLL_SPEED
+
+
+func _on_pipe_timer_timeout():
+	generate_pipes()
+	
+func generate_pipes():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + PIPE_DELAY
+	pipe.position.y = (screen_size.y - ground_height) / 2  + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	pipe.hit.connect(player_hit)
+	add_child(pipe)
+	pipes.append(pipe)
+	
+func player_hit():
+	$Player.falling = true
